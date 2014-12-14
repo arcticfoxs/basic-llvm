8e83e232b4cb30e06f45f9b06c166239bf05f575
David Hoare <dh464@cam.ac.uk>
2014-12-14 20:00:09 +0000

    lexings

--- BASICParser/Line.cs
+++ BASICParser/Line.cs
@@ -1,7 +1,34 @@
-﻿namespace BASICParser
+﻿using System.Collections.Generic;
+
+namespace BASICParser
 {
     class Line
     {
-        public int lineNumber;
+		public bool parsed = false;
+		public List<Symbol> lines;
+        public int lineNumber = -2;
+
+		public Line(List<Symbol> lexLines) {
+			lines = lexLines;
+		}
+
+		public void parse()
+		{
+			foreach (Symbol token in lines)
+			{
+				if (lineNumber == -2)
+				{
+					// No line number entered yet
+					if (token.symType == Symbol.sym.INTLITERAL)
+					{
+						lineNumber = token.intPayload; // Found a line number
+						continue;
+					}
+					else lineNumber = -1; // this line doesn't have a line number
+				}
+
+
+			}
+		}
     }
 }
--- BASICParser/Program.cs
+++ BASICParser/Program.cs
@@ -6,43 +6,12 @@ namespace BASICParser
 {
     class Program
     {
-        static List<Line> lines = new List<Line>();
         static void Main(string[] args)
         {
-            int counter = 0;
-            string line;
+			string inputFile = "D:\\basictest.txt"; // TODO: Read from parameter
 
-            // Read the file and display it line by line.
-            System.IO.StreamReader file = new System.IO.StreamReader("D:\\basictest.txt");
-            while ((line = file.ReadLine()) != null)
-            {
-                Console.WriteLine("(line " + counter.ToString()+")");
-                lexLine(line);
-                counter++;
-            }
-
-            file.Close();
-            // Suspend the screen.
-            Console.ReadLine();
+			Lexer.lex(inputFile);
         }
 
-        static void lexLine(string line)
-        {
-            StringReader reader = new StringReader(line);
-
-            Yylex lexer = new Yylex(reader);
-
-			Symbol nextSymbol = lexer.next_token();
-			while (nextSymbol != null)
-			{
-				if (nextSymbol.hasPayload)
-				{
-					if (nextSymbol.stringPayload != null) Console.WriteLine(nextSymbol.symType.ToString() + " " + nextSymbol.stringPayload.ToString());
-					else Console.WriteLine(nextSymbol.symType.ToString() + " " + nextSymbol.intPayload);
-				}
-				else Console.WriteLine(nextSymbol.symType);
-				nextSymbol = lexer.next_token();
-			}
-        }
     }
 }
