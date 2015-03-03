542f2a839b423ec22f5294f94112373227db81a6
David Hoare <dh464@cam.ac.uk>
2015-03-03 14:27:31 +0000

    PrintList

---
+++ BASICParser/PrintList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BASICLLVM
+{
+	class PrintList
+	{
+		public List<PrintItem> items;
+		public static enum printseparator { COMMA, SEMICOLON };
+
+		public List<printseparator> separators;
+
+		public PrintList()
+		{
+			items = new List<PrintItem>();
+		}
+
+		public void add(PrintItem item, printseparator separator)
+		{
+			items.Add(item);
+			separators.Add(separator);
+		}
+
+		public void add(PrintItem item)
+		{
+			items.Add(item);
+		}
+	}
+}
