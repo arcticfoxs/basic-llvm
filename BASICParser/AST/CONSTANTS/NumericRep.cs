d2b578470a21ebde8fde0fcac2a55585c7149ae8
David Hoare <dh464@cam.ac.uk>
2015-03-12 19:20:35 +0000

    numericrep llvm

--- BASICParser/AST/CONSTANTS/NumericRep.cs
+++ BASICParser/AST/CONSTANTS/NumericRep.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LLVM;
 
 namespace BASICLLVM.AST
 {
@@ -18,6 +14,11 @@ namespace BASICLLVM.AST
 		public NumericRep(Significand _significand)
 		{
 			significand = _significand;
+		}
+
+		public void code()
+		{
+
 		}
 	}
 }
